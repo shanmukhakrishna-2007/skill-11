d3b17784edd3080c41949343d212d4907f61f0bd
M.Shanmukha Krishna <shanmukhakrishna10@gmail.com>
2026-03-10 20:38:38 +0530

    Delete ex-frontend & backend directory

--- ex-frontend & backend/backend/src/main/java/com/klu/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.klu.repository;
-
-import com.klu.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String email);
-}
