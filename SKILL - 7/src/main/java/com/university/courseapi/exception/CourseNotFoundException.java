78a77968e15490c8fec0ca6642690562cbf05c45
M.Shanmukha Krishna <shanmukhakrishna10@gmail.com>
2026-03-10 20:34:23 +0530

    Delete SKILL - 7 directory

--- SKILL - 7/src/main/java/com/university/courseapi/exception/CourseNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.university.courseapi.exception;
-
-public class CourseNotFoundException extends RuntimeException {
-
-    public CourseNotFoundException(String message) {
-        super(message);
-    }
-}
